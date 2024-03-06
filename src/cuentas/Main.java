c8ff3bffd7ab89b839cc177f5ce96b71af6fed94
yasmi <yasmi@Yas>
2024-03-06 19:01:53 +0100

    Primer comit

--- src/cuentas/Main.java
+++ src/cuentas/Main.java
@@ -16,7 +16,7 @@ public class Main {
 
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         saldoActual = cuenta1.estado();
-        System.out.println("El saldo actual es"+ saldoActual );
+        System.out.println("El saldo actual es "+ saldoActual );
 
         try {
             cuenta1.retirar(2300);
